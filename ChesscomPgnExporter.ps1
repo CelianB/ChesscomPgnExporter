50a07da9cd5817cf254183b4782a47d4aacf46a0
CelianB <celian-bastien@outlook.fr>
2020-07-30 00:57:30 +0200

    readme

--- ChesscomPgnExporter.ps1
+++ ChesscomPgnExporter.ps1
@@ -1,16 +1,30 @@
+function SetTile{
+    $Host.UI.RawUI.WindowTitle = "ChesscomPgnExporter | $([char]0x00A9) C$([char]0x00E9)lian BASTIEN"
+}
+SetTile
+
 $user = Read-Host "Type your chess.com username"
+
 ## Download folder
 $outFolder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
 $outFile = "chess_com_games_$user.pgn"
 $outPath = Join-Path -Path $outFolder -ChildPath $outFile
-New-Item -Path $outPath -ItemType "file" -Force
-
+## Chesscom api endpoint
 $chessComArchivesUrl = "https://api.chess.com/pub/player/$user/games/archives"
 
+## File creation
+New-Item -Path $outPath -ItemType "file" -Force
+
+## Send the request
+try{
 $archivesReq = (Invoke-WebRequest -URI $chessComArchivesUrl | ConvertFrom-Json).archives 
+}
+catch{
+    Write-Error("Chesscom username is unknown")
+}
 $i =0
 foreach ($archive_url in $archivesReq) {
-    ## archive_url : format : https://api.chess.com/pub/player/user/games/yyyy/mm
+    ## archive_url : https://api.chess.com/pub/player/user/games/yyyy/mm
     $pourcent = ($i*100)/$archivesReq.length
     $splitter = $archive_url -split '/'
     $m = $splitter[-1]
@@ -18,7 +32,6 @@ foreach ($archive_url in $archivesReq) {
     Write-Progress -Activity Updating -Status "Downloading $m/$y" -PercentComplete $pourcent
     $i+=1
     $games = (Invoke-WebRequest -URI $archive_url | ConvertFrom-Json).games
-
     $u = 0
     foreach ($game in $games) {
         $subPourcent = ($u*100)/$games.length
@@ -28,3 +41,4 @@ foreach ($archive_url in $archivesReq) {
         $u+=1
     }
 }
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# ChesscomPgnExporter
+
+This powershell script allows you to export all your chess.com games in a single PGN file.
+
+Steps : 
+1. Download this repository (Code -> Download zip) / Clone if you have some git knowledges
+2. When the file ``ChesscomPgnExporter.ps1`` is on your PC, run it (On windows, right-click, click on ``run with Powershell``)
+3. Type your chesscom username, press ``<<Enter>>``, wait a bit
+4. A file named ``chess_com_games_username.pgn`` should be created in your ``Downloads`` folder
